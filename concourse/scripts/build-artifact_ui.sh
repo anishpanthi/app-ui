65096b447318daef3ec316d7d848ee01c71a0e76
Anish Panthi <anishpanthi@Anishs-MacBook-Pro.local>
2019-05-29 22:10:38 -0500

    Added ningx configuration

--- concourse/scripts/build-artifact_ui.sh
+++ concourse/scripts/build-artifact_ui.sh
@@ -8,5 +8,7 @@ npm run build
 
 cp package.json ../app-ui-dist
 #cp server.js ../app-ui-dist
+cp nginx.config ../app-ui-dist
+cp mime.types ../app-ui-dist
 cp -R ./dist ../app-ui-dist
 cp manifest.yml ../app-ui-dist
