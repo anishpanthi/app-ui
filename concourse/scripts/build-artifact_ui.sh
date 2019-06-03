9c0fb35f18d7d713bca63836497b9a6785215d62
Anish Panthi <anishpanthi@Anishs-MacBook-Pro.local>
2019-06-03 17:54:42 -0500

    Updated build

--- concourse/scripts/build-artifact_ui.sh
+++ concourse/scripts/build-artifact_ui.sh
@@ -6,9 +6,6 @@ cd git-repo
 npm install
 npm run build
 
-cp package.json ../app-ui-dist
-#cp server.js ../app-ui-dist
-cp nginx.config ../app-ui-dist
-cp mime.types ../app-ui-dist
-cp -R ./dist ../app-ui-dist
-cp manifest.yml ../app-ui-dist
+cp -R ./dist/* ../app-ui-dist/
+cp manifest.yml ../app-ui-dist/
+cp Staticfile ../app-ui-dist/
