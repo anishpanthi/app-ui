45554caead3d1b852a34f733480f33ca13e5ad53
Anish Panthi <anishpanthi@Anishs-MacBook-Pro.local>
2019-06-03 22:33:02 -0500

    Updated build script

--- concourse/scripts/build-artifact_ui_gulp.sh
+++ concourse/scripts/build-artifact_ui_gulp.sh
@@ -4,10 +4,10 @@ set -e
 #version=`cat version/number`
 cd git-repo
 npm install
-npm run build
+npm install gulp-cli -g
+npm install gulp -D
+gulp build
 
 cp -R ./dist/* ../app-ui-dist/
 cp manifest.yml ../app-ui-dist/
 cp Staticfile ../app-ui-dist/
-cp -R app-ui/* /app/public/
-rm -d app-ui
